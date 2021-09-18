3b593ffa76fa50d7e271d07d70b0c0e92448754f
mombasawalafaizan <64205100+mombasawalafaizan@users.noreply.github.com>
2021-09-18 11:15:08 +0530

    Delete build/lib/wrapup directory

--- build/lib/wrapup/__init__.py
+++
@@ -1,3 +0,0 @@
-from .master import rmv_print, convert_quotes
-
-__all__ = ["rmv_print", "convertQuotes"]
--- build/lib/wrapup/python_file_error.py
+++
@@ -1,7 +0,0 @@
-class NotAPythonFile(Exception):
-    """Exception raised when a given file is not a python file"""
-
-    def __init__(self, path) -> None:
-        self.path = path
-        self.message = self.path.name + " is not a python file."
-        super().__init__(self.message)
